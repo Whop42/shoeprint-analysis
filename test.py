82ac8ec06c5e797a99ef2e0ad9b37f7562132294
whop42 <whop42@icloud.com>
2023-01-24 22:49:07 -0800

    working without visualization of heightmap

--- test.py
+++ test.py
@@ -1,80 +1,28 @@
-# =============================================================================
-# PROJECT CHRONO - http://projectchrono.org
-#
-# Copyright (c) 2014 projectchrono.org
-# All rights reserved.
-#
-# Use of this source code is governed by a BSD-style license that can be found
-# in the LICENSE file at the top level of the distribution and at
-# http://projectchrono.org/license-chrono.txt.
-#
-# =============================================================================
-# Authors: Radu Serban
-# =============================================================================
-# Demo of the SCM semi-empirical model for deformable soil
-# =============================================================================
-
 import pychrono.core as chrono
 import pychrono.irrlicht as chronoirr
 import pychrono.vehicle as veh
 from PIL import Image
 import numpy as np
 
+
+import os, sys
 import math
+
+base_path = "data"
+filename = input(f"filename (in /{base_path}/objs/): ")
+file_path = os.path.join(base_path, "objs", filename)
+
+csv_path = os.path.join(base_path, "csvs", filename.replace(".obj", ".csv"))
 
 # The path to the Chrono data directory containing various assets (meshes, textures, data files)
 # is automatically set, relative to the default location of this demo.
 # If running from a different directory, you must change the path to the data directory with: 
-#chrono.SetChronoDataPath('path/to/data')
-
-# If true, use provided callback to change soil properties based on location
-var_params = False
-
-class MySoilParams (veh.SoilParametersCallback):
-    def __init__(self):
-        veh.SoilParametersCallback.__init__(self)
-    def Set(self, loc, Kphi, Kc, n, coh, mu_angle, shear, K, R):
-        Kphi_ = veh.doublep_value(Kphi)
-        Kc_ = veh.doublep_value(Kc)
-        n_ = veh.doublep_value(n)
-        coh_ = veh.doublep_value(coh)
-        mu_angle_ = veh.doublep_value(mu_angle)
-        shear_ = veh.doublep_value(shear)
-        K_ = veh.doublep_value(K)
-        R_ = veh.doublep_value(R)
-        if loc.y > 0 :
-            Kphi_ = 0.2e6
-            Kc_ = 0
-            n_ = 1.1
-            coh_ = 0
-            mu_angle_ = 30
-            shear_ = 0.01
-            K_ = 4e7
-            R_ = 3e4
-        else:
-            Kphi_ = 5301e3
-            Kc_ = 102e3
-            n_ = 0.793
-            coh_ = 1.3e3
-            mu_angle_ = 31.1
-            shear_ = 1.2e-2
-            K_ = 4e8
-            R_ = 3e4
-        veh.doublep_assign(Kphi, Kphi_)
-        veh.doublep_assign(Kc, Kc_)
-        veh.doublep_assign(n, n_)
-        veh.doublep_assign(coh, coh_)
-        veh.doublep_assign(mu_angle, mu_angle_)
-        veh.doublep_assign(shear, shear_)
-        veh.doublep_assign(K, K_)
-        veh.doublep_assign(R, R_)
-        
+# chrono.SetChronoDataPath("data")     
 
 # Global parameters for tire
 tire_rad = 0
 tire_vel_z0 = -3
 tire_center = chrono.ChVectorD(0, tire_rad, 0)
-# tire_w0 = tire_vel_z0 / tire_rad
 
 # ----------------------------
 # Create the mechanical system
@@ -95,7 +43,7 @@ body.SetPos(tire_center + chrono.ChVectorD(0, 0.2, 0))
 
 # Load mesh
 mesh = chrono.ChTriangleMeshConnected()
-mesh.LoadWavefrontMesh("test-shoe.obj")
+mesh.LoadWavefrontMesh(file_path)
 mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(0.2)) #scale
 mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(-(math.pi), chrono.ChVectorD(1, 0, 0))) # rotate
 
@@ -140,24 +88,20 @@ terrain_sizeX = 0.3
 terrain_sizeY = 0.75
 terrain_delta = 0.007/2
 terrain.Initialize(terrain_sizeX, terrain_sizeY, terrain_delta)
-my_params = MySoilParams()
-if var_params:
-    # Location-dependent soil properties
-    terrain.RegisterSoilParametersCallback(my_params)
-else :
-    # Constant soil properties
-    terrain.SetSoilParameters(2e6,  # Bekker Kphi
-                               0,      # Bekker Kc
-                               1.1,    # Bekker n exponent
-                               0,      # Mohr cohesive limit (Pa)
-                               30,     # Mohr friction limit (degrees)
-                               0.01,   # Janosi shear coefficient (m)
-                               2e8,    # Elastic stiffness (Pa/m), before plastic yield, must be > Kphi
-                               3e4     # Damping (Pa s/m), proportional to negative vertical speed (optional)
+
+terrain.SetSoilParameters(2e6,      # Bekker Kphi
+                            0,      # Bekker Kc
+                            1.1,    # Bekker n exponent
+                            0,      # Mohr cohesive limit (Pa)
+                            30,     # Mohr friction limit (degrees)
+                            0.01,   # Janosi shear coefficient (m)
+                            2e9,    # Elastic stiffness (Pa/m), before plastic yield, must be > Kphi
+                            3e4     # Damping (Pa s/m), proportional to negative vertical speed (optional)
     )
 
 # Set terrain visualization mode
 terrain.SetPlotType(veh.SCMDeformableTerrain.PLOT_PRESSURE_YELD, 0, 10000.2)
+
 # ------------------------------------------
 # Create the Irrlicht run-time visualization
 # ------------------------------------------
@@ -165,13 +109,14 @@ terrain.SetPlotType(veh.SCMDeformableTerrain.PLOT_PRESSURE_YELD, 0, 10000.2)
 vis = chronoirr.ChVisualSystemIrrlicht()
 vis.AttachSystem(sys)
 vis.SetWindowSize(1280,720)
-vis.SetWindowTitle('Shoeprint Testing')
+vis.SetWindowTitle(filename)
 vis.Initialize()
-# vis.AddLogo(chrono.GetChronoDataFile('logo_pychrono_alpha.png'))
 vis.AddSkyBox()
 vis.AddCamera(chrono.ChVectorD(0.25,0.1,0), chrono.ChVectorD(0,0,0))
 vis.AddTypicalLights()
 
+def normalize(value, _min, _max, norm_max):
+    return int((value - _min) / (_max - _min) * norm_max if _max - _min > 0 else 0)
 
 # -------------------------------
 # turn the terrain into a map png
@@ -180,42 +125,105 @@ vis.AddTypicalLights()
 def terrain_to_png(terrain: veh.SCMDeformableTerrain) -> None:
     mesh: chrono.ChTriangleMeshShape = terrain.GetMesh()
     mesh_mesh: chrono.ChTriangleMeshConnected = mesh.GetMesh()
-    vertices: chrono.ChVectorD = mesh_mesh.m_vertices
+    vertices: chrono.ChVectorD = mesh_mesh.getCoordsVertices()
     
+    # for v in vertices:
+    #     print(f"{v.x}, {v.y}, {v.z}")
+
+    # length of lists: width_vertices
+    # number of lists: height_vertices
+
+    # print(mesh_mesh.GetFileName())
 
     width_vertices = int(terrain_sizeX / terrain_delta)
     height_vertices = int(terrain_sizeY / terrain_delta)
 
-    # print human readable information about the height and width vertices and the ratios between them
-    count = 0
     heights = []
+    max_height = -100
+    min_height = 100
+
+    # while(len(vertices) > (width_vertices * height_vertices)):
+        
+
+    # vs = np.reshape(vertices, (width_vertices, height_vertices))
+
+    # for i in vs:
+    #     print(i.x)
+
+
     for i in range(0,height_vertices):
         heights.append([])
         for j in range(0, width_vertices):
-            loc: chrono.ChVectorD = vertices[((i-1)*j)+ j]
-            height = loc.z
-            count += 1
+            loc: chrono.ChVectorD = vertices[((i - 1) * j) + j]
+
+            if loc.y > max_height:
+                max_height = loc.y
+            if loc.y < min_height:
+                min_height = loc.y
+
+            height = loc.y
             heights[i].append(height)
 
-    print(heights)
-    print(count)
-    print(count / (width_vertices * height_vertices))
 
-    # normalize heights so that they are between 0 and 255
-    max_height = max(max(heights))
-    min_height = min(min(heights))
-    for i in range(len(heights)):
-        for j in range(len(heights[i])):
-            heights[i][j] = int(255 * (heights[i][j] - min_height) / (max_height - min_height))
-    print(heights)
+    # print(max_height)
+    # print(min_height)
 
-    # turn heights into a grayscale image that is a heightmap
-    img = Image.fromarray(np.uint8(heights), "L")
-    img.show()
+    # normalize the vertice coordinates to an image size
+    mins = [min([i.x for i in vertices]), min([i.y for i in vertices]), max([i.z for i in vertices])]
+    maxes = [max([i.x for i in vertices]), max([i.y for i in vertices]), max([i.z for i in vertices])]
+
+    normed_vertices = []
+    for vertex in vertices:
+        nx = normalize(vertex.x, mins[0], maxes[0], terrain_sizeX * 1000)
+        ny = normalize(vertex.y, mins[1], maxes[1], 255)
+        nz = normalize(vertex.z, mins[2], maxes[2], terrain_sizeY * 1000)
+
+        normed_vertices.append([nx, ny, nz])
+
+    # print(normed_vertices)
     
+    csv = open(csv_path, "w")
+    csv.flush()
+    csv.write("x, y, z\n")
+    for v in vertices:
+        csv.write(f"{v.x}, {v.y}, {v.z}\n")
+    csv.close()
 
-    img.save('terrain.png')
 
+    # final = []
+    # # for i in range(0, int(maxes[0] * 1000)):
+    # #     final.insert(i, [])
+
+    # print(final)
+
+    # for vertex in normed_vertices:
+    #     # final[vertex[0]][vertex[2]] = vertex[1]
+    #     final.insert(vertex[0], [])
+    #     print(vertex[0])
+    #     final[vertex[0]].insert(vertex[2], vertex[1])
+    #     # for row in final:
+    #     #     row.insert(vertex[2], vertex[1])
+    # print(final)
+
+    # # normalize heights so they are between 0 and 255
+    # # for i in range(0, height_vertices):
+    # #     for j in range(0, width_vertices):
+    # #         heights[i][j] = (heights[i][j] - min_height) / (max_height - min_height) * 255
+
+    # # turn heights into a grayscale image that is a heightmap
+    # img = Image.fromarray(np.uint8(final), "L")
+    # img.show()
+
+    # img.save('terrain.png')
+
+def export_mesh(terrain: veh.SCMDeformableTerrain):
+    mesh: chrono.ChTriangleMeshShape = terrain.GetMesh()
+    mesh_mesh: chrono.ChTriangleMeshConnected = mesh.GetMesh()
+    # chrono.ChTriangleMeshConnected_WriteWavefront("terrain.obj", mesh_mesh)
+
+    # print(type(chrono.vector_ChMesh(mesh_mesh)))
+    mesh_mesh.WriteWavefront("terrain.obj", mesh_mesh)
+    
 
 # ------------------
 # Run the simulation
@@ -226,15 +234,15 @@ frames = 0
 while vis.Run():
     if frames == 100:
         terrain_to_png(terrain)
-        # exit()
-        #terrain_to_png(terrain)
+        # export_mesh(terrain)
         body.SetCollide(False)
         vis_shape.SetVisible(False)
         mesh.Clear()
+        exit()
     
     
     vis.BeginScene()
-    # vis.GetSceneManager().getActiveCamera().setTarget(chronoirr.vector3dfCH(body.GetPos()))
+
     vis.Render()
 
     plane: chrono.ChCoordsysD = terrain.GetPlane()
