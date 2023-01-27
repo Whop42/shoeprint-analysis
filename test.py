54950df4d2d5cf13bd02440345cc5a881b35eec1
whop42 <whop42@icloud.com>
2023-01-26 16:40:55 -0800

    automatic multi-simulation

--- test.py
+++ test.py
@@ -11,132 +11,30 @@ import math
 ### CHANGE THESE VALUES ###
 
 # name of file in /data/objs/
-filename = "1.obj"
+filename = "11.obj"
 
 # change this value until the mesh fits (after rotation)
-mesh_scale = 0.4
+mesh_scale = 0.17
 
 # change this until the mesh is rotated properly (it's in radians)
-mesh_rotation = math.pi/3.5
+mesh_rotation = math.pi
 
 # the frame at which to end (don't give it time to jiggle around)
-frame_stop = 110 # set this so it stops once the print solidly hits the terrain
+frame_stop = 105 # set this so it stops once the print solidly hits the terrain
 
 # the degrees to rotate over flipping the print
 # (for if you want it to not land in the middle of the print)
 flip_extra = 0*math.pi
 
+# offset the mesh (sometimes you just can't be bothered to edit it again, yk)
+offset = [0.02, 0, 0]
+
+# automatic mode: do an entire folder (as long as they don't have "-scan")
+automatic_mode = True
+
 ###########################
 ###########################
 ###########################
-
-base_path = "data"
-# filename = input(f"filename (in /{base_path}/objs/): ")
-file_path = os.path.join(base_path, "objs", filename)
-
-csv_path = os.path.join(base_path, "csvs", filename.replace(".obj", ".csv"))
-
-# The path to the Chrono data directory containing various assets (meshes, textures, data files)
-# is automatically set, relative to the default location of this demo.
-# If running from a different directory, you must change the path to the data directory with: 
-# chrono.SetChronoDataPath("data")     
-
-# Global parameters for tire
-tire_rad = 0
-tire_vel_z0 = -3
-tire_center = chrono.ChVectorD(0, tire_rad, 0)
-
-# ----------------------------
-# Create the mechanical system
-# ----------------------------
-
-sys = chrono.ChSystemSMC()
-
-# Create the ground
-ground = chrono.ChBody()
-ground.SetBodyFixed(True)
-sys.Add(ground)
-
-# Create the rigid body with contact mesh
-body = chrono.ChBody()
-sys.Add(body)
-body.SetMass(50)
-body.SetPos(tire_center + chrono.ChVectorD(0, 0.2, 0))
-
-# Load mesh
-mesh = chrono.ChTriangleMeshConnected()
-mesh.LoadWavefrontMesh(file_path)
-mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(mesh_scale)) # scale
-mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(-(math.pi) + flip_extra, chrono.ChVectorD(1, 0, 0))) # flip
-mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(mesh_rotation, chrono.ChVectorD(0, 1, 0)))
-
-# Set visualization assets
-vis_shape = chrono.ChTriangleMeshShape()
-vis_shape.SetMesh(mesh)
-# vis_shape.SetColor(chrono.ChColor(0.3, 0.3, 0.3))
-vis_shape.SetWireframe(True)
-body.AddVisualShape(vis_shape)
-
-# Set collision shape
-material = chrono.ChMaterialSurfaceSMC()
-
-body.GetCollisionModel().ClearModel()
-body.GetCollisionModel().AddTriangleMesh(material,                # contact material
-                                         mesh,                    # the mesh 
-                                         False,                   # is it static?
-                                         False,                   # is it convex?
-                                         chrono.ChVectorD(0,0,0), # position on body
-                                         chrono.ChMatrix33D(1),   # orientation on body 
-                                         0.01)                    # "thickness" for increased robustness
-body.GetCollisionModel().BuildModel()
-body.SetCollide(True)
-
-# Create motor
-# motor = chrono.ChLinkMotorRotationAngle()
-# motor.SetSpindleConstraint(chrono.ChLinkMotorRotation.SpindleConstraint_OLDHAM)
-# motor.SetAngleFunction(chrono.ChFunction_Ramp(0, math.pi / 4))
-# motor.Initialize(body, ground, chrono.ChFrameD(tire_center, chrono.Q_from_AngY(math.pi/2)))
-# sys.Add(motor)
-
-# ------------------------
-# Create SCM terrain patch
-# ------------------------
-
-# Note that SCMDeformableTerrain uses a default ISO reference frame (Z up). Since the mechanism is modeled here in
-# a Y-up global frame, we rotate the terrain plane by -90 degrees about the X axis.
-terrain = veh.SCMDeformableTerrain(sys)
-terrain.SetPlane(chrono.ChCoordsysD(chrono.ChVectorD(0,0,0), chrono.Q_from_AngX(-math.pi/2)))
-
-terrain_sizeX = 0.3
-terrain_sizeY = 0.75
-terrain_delta = 0.007/2
-terrain.Initialize(terrain_sizeX, terrain_sizeY, terrain_delta)
-
-terrain.SetSoilParameters(2e6,      # Bekker Kphi
-                            0,      # Bekker Kc
-                            1.1,    # Bekker n exponent
-                            0,      # Mohr cohesive limit (Pa)
-                            30,     # Mohr friction limit (degrees)
-                            0.01,   # Janosi shear coefficient (m)
-                            2e9,    # Elastic stiffness (Pa/m), before plastic yield, must be > Kphi
-                            3e4     # Damping (Pa s/m), proportional to negative vertical speed (optional)
-    )
-
-# Set terrain visualization mode
-terrain.SetPlotType(veh.SCMDeformableTerrain.PLOT_PRESSURE_YELD, 0, 10000.2)
-
-# ------------------------------------------
-# Create the Irrlicht run-time visualization
-# ------------------------------------------
-
-vis = chronoirr.ChVisualSystemIrrlicht()
-vis.AttachSystem(sys)
-vis.SetWindowSize(500,500)
-vis.SetWindowTitle(filename)
-vis.Initialize()
-vis.AddSkyBox()
-vis.AddCamera(chrono.ChVectorD(0.25,0.1,0), chrono.ChVectorD(0,0,0))
-vis.AddTypicalLights()
 
 def normalize(value, _min, _max, norm_max):
     return int((value - _min) / (_max - _min) * norm_max if _max - _min > 0 else 0)
@@ -247,31 +145,150 @@ def export_mesh(terrain: veh.SCMDeformableTerrain):
 
     # print(type(chrono.vector_ChMesh(mesh_mesh)))
     mesh_mesh.WriteWavefront("terrain.obj", mesh_mesh)
+
+base_path = "data"
+
+filenames = [filename]
+
+if automatic_mode:
+    for f in os.listdir(os.path.join(base_path, "objs")):
+        if not "scan" in f and f != filenames[0] and ".obj" in f and "empty" not in f:
+            filenames.append(f)
+
+for filename in filenames:
+    print("starting " + filename)
+    # filename = input(f"filename (in /{base_path}/objs/): ")
+    file_path = os.path.join(base_path, "objs", filename)
+
+    csv_path = os.path.join(base_path, "csvs", filename.replace(".obj", ".csv"))
+
+    # The path to the Chrono data directory containing various assets (meshes, textures, data files)
+    # is automatically set, relative to the default location of this demo.
+    # If running from a different directory, you must change the path to the data directory with: 
+    # chrono.SetChronoDataPath("data")     
+
+    # Global parameters for tire
+    tire_rad = 0
+    tire_vel_z0 = -3
+    tire_center = chrono.ChVectorD(0, tire_rad, 0)
+
+    # ----------------------------
+    # Create the mechanical system
+    # ----------------------------
+
+    sys = chrono.ChSystemSMC()
+
+    # Create the ground
+    ground = chrono.ChBody()
+    ground.SetBodyFixed(True)
+    sys.Add(ground)
+
+    # Create the rigid body with contact mesh
+    body = chrono.ChBody()
+    sys.Add(body)
+    body.SetMass(50)
+    body.SetPos(tire_center + chrono.ChVectorD(0, 0.2, 0))
+
+    # Load mesh
+    mesh = chrono.ChTriangleMeshConnected()
+    mesh.LoadWavefrontMesh(file_path)
+    mesh.Transform(chrono.ChVectorD(offset[0],offset[1],offset[2]), chrono.ChMatrix33D(mesh_scale)) # scale
+    mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(-(math.pi) + flip_extra, chrono.ChVectorD(1, 0, 0))) # flip
+    mesh.Transform(chrono.ChVectorD(0,0,0), chrono.ChMatrix33D(mesh_rotation, chrono.ChVectorD(0, 1, 0)))
+
+    # Set visualization assets
+    vis_shape = chrono.ChTriangleMeshShape()
+    vis_shape.SetMesh(mesh)
+    # vis_shape.SetColor(chrono.ChColor(0.3, 0.3, 0.3))
+    vis_shape.SetWireframe(True)
+    body.AddVisualShape(vis_shape)
+
+    # Set collision shape
+    material = chrono.ChMaterialSurfaceSMC()
+
+    body.GetCollisionModel().ClearModel()
+    body.GetCollisionModel().AddTriangleMesh(material,                # contact material
+                                            mesh,                    # the mesh 
+                                            False,                   # is it static?
+                                            False,                   # is it convex?
+                                            chrono.ChVectorD(0,0,0), # position on body
+                                            chrono.ChMatrix33D(1),   # orientation on body 
+                                            0.01)                    # "thickness" for increased robustness
+    body.GetCollisionModel().BuildModel()
+    body.SetCollide(True)
+
+    # Create motor
+    # motor = chrono.ChLinkMotorRotationAngle()
+    # motor.SetSpindleConstraint(chrono.ChLinkMotorRotation.SpindleConstraint_OLDHAM)
+    # motor.SetAngleFunction(chrono.ChFunction_Ramp(0, math.pi / 4))
+    # motor.Initialize(body, ground, chrono.ChFrameD(tire_center, chrono.Q_from_AngY(math.pi/2)))
+    # sys.Add(motor)
+
+    # ------------------------
+    # Create SCM terrain patch
+    # ------------------------
+
+    # Note that SCMDeformableTerrain uses a default ISO reference frame (Z up). Since the mechanism is modeled here in
+    # a Y-up global frame, we rotate the terrain plane by -90 degrees about the X axis.
+    terrain = veh.SCMDeformableTerrain(sys)
+    terrain.SetPlane(chrono.ChCoordsysD(chrono.ChVectorD(0,0,0), chrono.Q_from_AngX(-math.pi/2)))
+
+    terrain_sizeX = 0.3
+    terrain_sizeY = 0.75
+    terrain_delta = 0.007/2
+    terrain.Initialize(terrain_sizeX, terrain_sizeY, terrain_delta)
+
+    terrain.SetSoilParameters(2e6,      # Bekker Kphi
+                                0,      # Bekker Kc
+                                1.1,    # Bekker n exponent
+                                0,      # Mohr cohesive limit (Pa)
+                                30,     # Mohr friction limit (degrees)
+                                0.01,   # Janosi shear coefficient (m)
+                                2e9,    # Elastic stiffness (Pa/m), before plastic yield, must be > Kphi
+                                3e4     # Damping (Pa s/m), proportional to negative vertical speed (optional)
+        )
+
+    # Set terrain visualization mode
+    terrain.SetPlotType(veh.SCMDeformableTerrain.PLOT_PRESSURE_YELD, 0, 10000.2)
+
+    # ------------------------------------------
+    # Create the Irrlicht run-time visualization
+    # ------------------------------------------
+
+    vis = chronoirr.ChVisualSystemIrrlicht()
+    vis.AttachSystem(sys)
+    vis.SetWindowSize(500,500)
+    vis.SetWindowTitle(filename)
+    vis.Initialize()
+    vis.AddSkyBox()
+    vis.AddCamera(chrono.ChVectorD(0.25,0.1,0), chrono.ChVectorD(0,0,0))
+    vis.AddTypicalLights()
+
+    # ------------------
+    # Run the simulation
+    # ------------------
+
+    frames = 0
+
+    while vis.Run():
+        if frames == frame_stop:
+            terrain_to_png(terrain)
+            # export_mesh(terrain)
+            body.SetCollide(False)
+            vis_shape.SetVisible(False)
+            mesh.Clear()
+            vis.GetGUIEnvironment()
+            vis.Initialize()
+            break
+        
+        vis.BeginScene()
+
+        vis.Render()
+
+        plane: chrono.ChCoordsysD = terrain.GetPlane()
+        
+
+        vis.EndScene()
+        sys.DoStepDynamics(0.002)
+        frames += 1
     
-
-# ------------------
-# Run the simulation
-# ------------------
-
-frames = 0
-
-while vis.Run():
-    if frames == frame_stop:
-        terrain_to_png(terrain)
-        # export_mesh(terrain)
-        body.SetCollide(False)
-        vis_shape.SetVisible(False)
-        mesh.Clear()
-        exit()
-    
-    
-    vis.BeginScene()
-
-    vis.Render()
-
-    plane: chrono.ChCoordsysD = terrain.GetPlane()
-    
-
-    vis.EndScene()
-    sys.DoStepDynamics(0.002)
-    frames += 1
